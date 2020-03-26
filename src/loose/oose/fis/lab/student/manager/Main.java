928fe51511dabb0b264ec459d96beae9939b1789
olariudaiana <62403723+olariudaiana@users.noreply.github.com>
2020-03-26 19:19:13 +0200

    SM - 1 Added the login screen

--- src/loose/oose/fis/lab/student/manager/Main.java
+++ src/loose/oose/fis/lab/student/manager/Main.java
@@ -1,13 +1,24 @@
-@Override
+package loose.oose.fis.lab.student.manager;
 
-public void start(Stage primaryStage) throws Exception {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("fxml/login.fxml"));
-
         primaryStage.setTitle("FIS - Student Manager");
-
         primaryStage.setScene(new Scene(root, 600, 400));
-
         primaryStage.show();
+    }
 
-        }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
